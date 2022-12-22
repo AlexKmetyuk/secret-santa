7b4299a812b97f6cd272377edd4d4a5978c007a5
Alexander Kmetyuk <kmetyuk.sasha@gmail.com>
2022-12-22 18:38:42 +0200

    save

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -18,7 +18,6 @@ export function UserInfo({ user }) {
     try {
       setShowLoader(true);
       const res = await api.setWishes(wish);
-      console.log("Wish res:", res.user);
       setUserWish(res.user.wish);
       setShowLoader(false);
     } catch (error) {
@@ -44,13 +43,7 @@ export function UserInfo({ user }) {
   };
 
   useEffect(() => {
-    console.log(userWish);
-  }, [userWish]);
-
-  useEffect(() => {
     api.getUsersCount().then((res) => {
-      console.log("res", res);
-
       if (res.usersLength >= 16) {
         setAllUser(true);
       } else {
@@ -61,7 +54,6 @@ export function UserInfo({ user }) {
       api
         .getLuckyWish()
         .then((res) => {
-          console.log(res);
           if (res.userWish) {
             setLuckyWish(res.userWish);
           }
@@ -81,25 +73,28 @@ export function UserInfo({ user }) {
           <div>
             <p className="user-info__name">Hello, {user.name}!</p>
             <div className="wishes">
-              {userWish || user.wish ? (
-                <p className="user-info__wish">
-                  Your wish is - "{userWish || user.wish}"
-                </p>
-              ) : (
-                <>
+              <>
+                {wish || user.wish ? (
+                  <label className="wishes__label">
+                    Change your wishe here:
+                  </label>
+                ) : (
                   <label className="wishes__label">
                     Enter your wishes here:
                   </label>
-                  <textarea
-                    className="wishes__input"
-                    onChange={(e) => {
-                      setWish(e.target.value);
-                    }}
-                    value={wish}
-                  />
-                  <Button title="Submit" onClick={addWishes} />{" "}
-                </>
-              )}
+                )}
+                <textarea
+                  className="wishes__input"
+                  onChange={(e) => {
+                    setWish(e.target.value);
+                    if (user.wish) {
+                      user.wish = e.target.value;
+                    }
+                  }}
+                  value={wish || user.wish}
+                />
+                <Button title="Submit" onClick={addWishes} />{" "}
+              </>
             </div>
           </div>
           {isAllUsers && (
